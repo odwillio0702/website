a5e9dd72abf45dc51ed56fd4ae5273584853a453
odwillio0702 <odwillio21100@gmail.com>
2024-12-05 10:13:32 +0100

    1

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// Get the video
-let video = document.getElementById("myVideo");
-
-// Get the button
-let btn = document.getElementById("myBtn");
-
-// Pause and play the video, and change the button text
-function myFunction() {
-   if (video.paused) {
-      video.play();
-      btn.innerHTML = "Pause";
-   } else {
-      video.pause();
-      btn.innerHTML = "Play";
-   }
-}
+document.addEventListener("DOMContentLoaded", () => {
+   console.log("Добро пожаловать в мир Minecraft!");
+ });
+ 
