d2fc316c1c6bc94d620e2cb97c243bba6c89ad1f
odwillio0702 <odwillio21100@gmail.com>
2024-12-05 10:26:05 +0100

    1

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-   console.log("Добро пожаловать!");
+   const themeToggle = document.getElementById("theme-toggle");
+ 
+   themeToggle.addEventListener("click", () => {
+     document.body.classList.toggle("light-theme");
+     const isLight = document.body.classList.contains("light-theme");
+     localStorage.setItem("theme", isLight ? "light" : "dark");
+   });
+ 
+   // Установка темы из локального хранилища
+   if (localStorage.getItem("theme") === "light") {
+     document.body.classList.add("light-theme");
+   }
  });
  
